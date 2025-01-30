92d64a1b6614e3189a6f733129b01801d1b4fc76
Khan_San <Khan_San@KLIN0078>
2025-01-30 10:48:27 +0530

    Add project files.

---
+++ MachineAssetTracker/Models/MachineAsset.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MachineAssetTracker.Models
+{
+    public class MachineAsset
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = MongoDB.Bson.ObjectId.GenerateNewId().ToString();
+
+        public string MachineType { get; set; }= string.Empty;
+        public string Asset { get; set; } = string.Empty;
+        public string Series { get; set; } = string.Empty;
+
+
+    }
+}
